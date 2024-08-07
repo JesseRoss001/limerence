ad6a6ff2ddad6f17cddb8cdda35d44430aba8f0d
Jesse Lee Ross <79084912+JesseRoss001@users.noreply.github.com>
2024-08-07 20:10:14 +0100

    update

--- limerance_project/settings.py
+++ limerance_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'limerance_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # Password validation
