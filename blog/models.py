c087cc74a66dbf15b978cf6fba78de91498b1146
Jesse Lee Ross <79084912+JesseRoss001@users.noreply.github.com>
2024-09-30 13:17:37 +0100

    update

--- blog/models.py
+++ blog/models.py
@@ -7,9 +7,13 @@ class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
     body = RichTextField(blank=True)
+    meta_title = models.CharField(max_length=100, blank=True, help_text="SEO meta title for this blog post")
+    meta_description = models.CharField(max_length=160, blank=True, help_text="SEO meta description for this blog post")
 
     content_panels = Page.content_panels + [
         FieldPanel('date'),
         FieldPanel('intro'),
         FieldPanel('body'),
+        FieldPanel('meta_title'),
+        FieldPanel('meta_description'),
     ]
